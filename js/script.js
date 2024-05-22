3128bb934222288ba3887760db982169226af8ca
AsaMalika25 <khairulasa9@gmail.com>
2024-05-22 07:26:32 +0700

    initiazing portofolio

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function (){
+
+    $('#menu').click(function () {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+    
+    $(window).on('scroll load', function () {
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if (window.scrollY > 60) {
+            document.querySelector('#scroll-top').classList.add('active');
+        } else {
+            document.querySelector('#scroll-top').classList.remove('active');
+        }
+    });
+})
